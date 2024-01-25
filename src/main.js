fe28925913b81829454404ca2740e26636022cdc
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-01-25 01:57:46 -0800

    head comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+/* Head comment:
+* Author: Jaxon Ruiz
+* Mod Name: "Better than League of Legends"
+* approximately 7 hours, but heavy guess-imating
+* Mod List:
+(3 points) Dynamic difficulty in enemy ships, where every hit increases the ship’s speed and points
+(for balancing, the player rocket’s horizontal move speed was increased to 3.5, while keeping the shot speed at 2
+(1 point) Allow the player to control the Rocket after it's fired 
+However while firing, the rocket can only move at 1/8 of its normal speed
+(3 points) display remaining timer on scene
+(5 points) Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses
+(5 points) Special ship type: UFO. slides back and forth, is smaller than other spaceships, moves with a variable speed (faster on average), and is worth 50 points on default
+(3 points) parallax scrolling with streaks and planet objects.
+*/
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
